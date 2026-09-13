22bf570bb0a45d5bc344acc11bb8a3fac31fcadb
Family <Family@Saharshs-MacBook-Air.local>
2026-09-12 19:25:26 -0500

    app name updated

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="my_app",  # replace with your actual app package name
+    app_name="myreflex_app",  # replace with your actual app package name
     api_url="https://myreflex-app.onrender.com",
     cors_allowed_origins=[
         "https://myreflex-app.onrender.com",
